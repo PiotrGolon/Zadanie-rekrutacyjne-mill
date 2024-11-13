57245b8f4dee2c5e9941aa5038ceb1a7c2c5983d
Piotr Golon <piotrek98golon@o2.pl>
2024-11-13 13:25:37 +0100

    get transaction comment removed

--- client/bank-transactions/src/features/transactions/api/use-get-transactions.ts
+++ client/bank-transactions/src/features/transactions/api/use-get-transactions.ts
@@ -13,13 +13,11 @@ export const useGetTransactions = () => {
 
       const data: Transaction[] = await response.json();
 
-      // Dodanie pola type na podstawie amount
       const transactionsWithType: Transaction[] = data.map((transaction) => ({
         ...transaction,
         type: transaction.amount >= 0 ? "income" : "expense",
       }));
 
-      // Sortowanie transakcji według daty w kolejności malejącej (najpierw najnowsze)
       const sortedTransactions = [...transactionsWithType].sort((a, b) => {
         const dateA = new Date(a.date).getTime();
         const dateB = new Date(b.date).getTime();
